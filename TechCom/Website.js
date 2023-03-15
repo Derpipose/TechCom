c8d738a0cdf9fe943625234e99dcea228a95b6ae
Stormgate1998 <jalapenojosh@gmail.com>
2023-03-15 17:36:24 -0600

    Fixed stuff, including formatting

--- TechCom/Website.js
+++ TechCom/Website.js
@@ -1,7 +1,7 @@
-window.onscroll = function() {
+window.onscroll = function () {
     var lifeBar = document.getElementById('life-bar');
     var scrollPosition = window.scrollY;
     var maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var lifePercentage = (1 - scrollPosition / maxScroll) * 100;
     lifeBar.style.width = lifePercentage + '%';
-  }
+}
