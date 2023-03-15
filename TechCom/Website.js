87d716dbb91b26fe43f97e0848732761041885db
Derpipose <thefluffmaster2000@gmail.com>
2023-03-15 17:30:01 -0600

    I ADDED A HELTH BR

--- TechCom/Website.js
+++ TechCom/Website.js
@@ -1,11 +1,7 @@
-const healthBar = document.querySelector('.health-bar');
-
-window.addEventListener('scroll', () => {
-  const scrollTop = document.documentElement.scrollTop;
-  const scrollHeight = document.documentElement.scrollHeight;
-  const clientHeight = document.documentElement.clientHeight;
-
-  const percentScrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
-
-    healthBar.style.height = `${100 - percentScrolled}%`;
-});
+window.onscroll = function() {
+    var lifeBar = document.getElementById('life-bar');
+    var scrollPosition = window.scrollY;
+    var maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var lifePercentage = (1 - scrollPosition / maxScroll) * 100;
+    lifeBar.style.width = lifePercentage + '%';
+  }
